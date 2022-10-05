c26fe388a0ffaba56ace5e25e0a689fbacee5d24
Marcos Paulo <itsolutions031250@gmail.com>
2022-10-05 09:54:31 -0300

    entities_abstracts_classes

---
+++ src/entities/Circle.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Circle {
+
+    private Double radius;
+
+    public Circle(Double radius) {
+        this.radius = radius;
+    }
+
+    public Double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(Double radius) {
+        this.radius = radius;
+    }
+
+
+}
